28fc5fd43b1d391b2793514163f3090d9a9d9b44
hgalocy <hannahgalocy@Hannahs-MBP.lan>
2023-01-30 18:46:01 -0800

    get node require working

--- functionality/index.js
+++ functionality/index.js
@@ -1,8 +1,11 @@
 const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
+const { document } = (new JSDOM('')).window;
+global.document = document;
+// const $ = require( "jquery" )( window );
+var $ = jQuery = require('jquery')(window);
 
-jQuery(document).ready(function ($) {
+$(document).ready(function ($) {
     $("#header").load("header.html"); 
 });
 // $("#footer").load("footer.html"); 
